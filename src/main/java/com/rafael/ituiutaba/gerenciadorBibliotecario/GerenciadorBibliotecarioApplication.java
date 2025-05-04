1e1d9f6d512f3d3378db93f907cc6430851eb2d7
Rafael Pacífico <rafaguitar177@gmail.com>
2025-05-04 11:37:14 -0300

    Gravando inicio do projeto

---
+++ src/main/java/com/rafael/ituiutaba/gerenciadorBibliotecario/GerenciadorBibliotecarioApplication.java
@@ -0,0 +1,15 @@
+package com.rafael.ituiutaba.gerenciadorBibliotecario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorBibliotecarioApplication {
+
+	//  para acessar a aplicação pelo browser
+    //  http://localhost:8080/
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorBibliotecarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/ituiutaba/gerenciadorBibliotecario/GerenciadorBibliotecarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.ituiutaba.gerenciadorBibliotecario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorBibliotecarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
